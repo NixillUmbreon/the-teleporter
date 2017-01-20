8c6a89f5de3f149886fd58fb91b1b88c3036b1b9
RainbowEevee <raitheeevee@gmail.com>
2017-01-20 00:43:24 -0500

    Upload blank plugin

---
+++ TheTeleporter/src/net/nixill/teleporter/TheTeleporter.java
@@ -0,0 +1,11 @@
+package net.nixill.teleporter;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class TheTeleporter extends JavaPlugin {
+  @Override
+  public void onEnable() { }
+  
+  @Override
+  public void onDisable() { }
+}
